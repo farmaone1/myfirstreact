bf1427eb1b137c85a1c4da0a5327d02472d1edac
pupkinzalupkin228 <pupkinzalupkin228@gmail.com>
2024-12-24 12:33:03 +0300

    Displaying Critic score

--- src/components/PlatformIconsList.tsx
+++ src/components/PlatformIconsList.tsx
@@ -1,11 +1,9 @@
 import { Platform } from '@/hooks/useGames'
-import { HStack, Icon, Text } from '@chakra-ui/react';
+import {  HStack } from '@chakra-ui/react';
 import { FaWindows, FaPlaystation, FaXbox, FaApple, FaLinux, FaAndroid} from 'react-icons/fa';
 import { MdPhoneIphone } from 'react-icons/md';
 import { SiNintendo } from 'react-icons/si';
 import { BsGlobe } from 'react-icons/bs';
-import { IconType } from 'react-icons';
-import React from 'react';
 
 interface Props {
     platforms : Platform[];
@@ -15,22 +13,21 @@ interface Props {
 
 const PlatformIconsList = ({ platforms }: Props) => {
 
-  const iconsMap : {[key:string] : any} = {
-    pc: FaWindows,
-    playstation: FaPlaystation,
-    xbox: FaXbox,
-    nintendo: SiNintendo,
-    mac: FaApple,
-    linux:  FaLinux,
-    android: FaAndroid,
-    iphone: MdPhoneIphone,
-    web: BsGlobe
+  const iconsMap = {
+    "pc": <FaWindows/>,
+    "playstation": <FaPlaystation/>,
+    "xbox": <FaXbox/>,
+    "nintendo": <SiNintendo/>,
+    "mac": <FaApple/>,
+    "linux": < FaLinux/>,
+    "android": <FaAndroid/>,
+    "iphone": <MdPhoneIphone/>,
+    "web": <BsGlobe/>
   }
   return (
-    <HStack marginY={'10px'}>
-        
+    <HStack marginY={'10px'} color={"gray.500"}>
         {platforms.map((platform) => {
-            return <Icon color="gray.500"><FaWindows/></Icon>
+            return <>{iconsMap[platform.slug as keyof typeof iconsMap]}</>;
         })}  
     </HStack> 
   )
