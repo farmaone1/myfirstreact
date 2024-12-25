dce2622e36d5901a53164bcbba5d600082f336e5
pupkinzalupkin228 <pupkinzalupkin228@gmail.com>
2024-12-25 19:28:03 +0300

    Sort the games

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,14 @@ import SortSelector from "./components/SortSelector";
 export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
+  sortOrder: string;
 }
 
 
 function App() {
 
-  // const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
-  // const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null);
   const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
+
   return(
         <Grid templateAreas={{
           base: `"nav" "main"`,
@@ -40,7 +40,7 @@ function App() {
 
           <GridItem area='main'>
               <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({...gameQuery, platform})}/>
-              <SortSelector />
+              <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({...gameQuery, sortOrder})}/>
             <GameGrid gameQuery={gameQuery}/>
           </GridItem>
         </Grid>
--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,21 +1,32 @@
 import { Button, MenuContent, MenuItem, MenuRoot, MenuTrigger } from '@chakra-ui/react'
 import { BsChevronDown } from 'react-icons/bs'
 
-const SortSelector = () => {
+interface Props {
+    onSelectSortOrder: (sortOrder: string) => void;
+    sortOrder: string;
+}
+
+const SortSelector = ({onSelectSortOrder, sortOrder}: Props) => {
+
+    const sortOrders = [
+        { value: '', label: 'Relevance' },
+        { value: '-added', label: 'Data added' },
+        { value: 'name', label: 'Name' },
+        { value: '-released', label: 'Release date' },
+        { value: '-metacritic', label: 'Popularity' },
+        { value: '-rating', label: 'Average rating' },
+    ]
+
+    const currentSortOrder = sortOrders.find(order => order.value === sortOrder)
   return (
       <MenuRoot>
           <MenuTrigger asChild marginX={'10px'} marginBottom={5}>
               <Button variant="outline" size="sm">
-                 <BsChevronDown /> Order by: Relevance
+                 <BsChevronDown /> Order by: {currentSortOrder?.label || 'Relevance'}
               </Button>
         </MenuTrigger>
         <MenuContent marginX={'147px'} position={'absolute'}>
-           <MenuItem value="">Relevance</MenuItem>
-           <MenuItem value="">Date added</MenuItem>
-           <MenuItem value="">Name</MenuItem>
-           <MenuItem value="">Release day</MenuItem>
-           <MenuItem value="">Popularity</MenuItem>
-           <MenuItem value="">Average rating</MenuItem>
+           {sortOrders.map(order => <MenuItem onClick={() => onSelectSortOrder(order.value)} key={order.value} value={order.label}>{order.label}</MenuItem>)}
         </MenuContent>
       </MenuRoot>
     );
