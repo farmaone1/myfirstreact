a2374546aba92675f637fb5f7bc1352e3f3c4261
pupkinzalupkin228 <pupkinzalupkin228@gmail.com>
2024-12-25 17:11:44 +0300

    Filter games by genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,11 +3,16 @@ import { SimpleGrid, Text } from '@chakra-ui/react';
 import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
 import GameCardContainer from './GameCardContainer';
+import { Genre } from '@/hooks/useGenres';
+
+interface Props {
+    selectedGenre: Genre | null;
+}
 
 
+const GameGrid = ({selectedGenre}: Props) => {
 
-const GameGrid = () => {
-  const {data, error, isLoading} = useGames();
+  const {data, error, isLoading} = useGames(selectedGenre);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
   return (
@@ -15,12 +20,12 @@ const GameGrid = () => {
         {error && <Text>{error}</Text>}
         <SimpleGrid columns={{sm: 1, md:2, lg:3, xl: 5}} gap={3} padding={'10px'}>
             {isLoading && skeletons.map(skeleton => 
-                <GameCardContainer>
-                    <GameCardSkeleton key={skeleton}/>
+                <GameCardContainer key={skeleton}>
+                    <GameCardSkeleton />
                 </GameCardContainer>)}
             {data.map(game => 
-                <GameCardContainer>
-                    <GameCard key={game.id} game={game}/>
+                <GameCardContainer key={game.id}>
+                    <GameCard game={game}/>
                 </GameCardContainer>
             )}
         </SimpleGrid>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,22 +1,26 @@
-import useGenres from '@/hooks/useGenres'
+import useGenres, { Genre } from '@/hooks/useGenres'
 import getCroppedImageUrl from '@/services/image-url';
-import { HStack, Image, List, Spinner, Text } from '@chakra-ui/react';
+import { Button, HStack, Image, List, Spinner  } from '@chakra-ui/react';
 
-const GenreList = () => {
+interface Props {
+    onSelectGenre: (genre: Genre) => void;
+}
+
+const GenreList = ({onSelectGenre}: Props) => {
   const { data, isLoading, error } = useGenres();
   if (error) return null;
   if (isLoading) return <Spinner/>;
-  
+
   return (
     <List.Root>
         {data.map(genre => (
         <List.Item key={genre.id} paddingY={'5px'} listStyle="none">
             <HStack>
                 <Image boxSize={'32px'} src={getCroppedImageUrl(genre.image_background)} borderRadius={8}/>
-                <Text fontSize={'lg'}>{genre.name}</Text>
+                <Button fontSize={'lg'} variant='ghost' onClick={() => onSelectGenre(genre)}>{genre.name}</Button>
             </HStack> 
         </List.Item>))}
-    </List.Root>
+    </List.Root> 
   ) 
 }
 
