174ec459aade6ad6517c03d75a5a17ed10035d71
pupkinzalupkin228 <pupkinzalupkin228@gmail.com>
2024-12-25 20:58:54 +0300

    Customize the theme

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,30 @@ import './index.css'
 import App from './App.tsx'
 
 import { ColorModeProvider } from "@/components/ui/color-mode"
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+import { ChakraProvider, createSystem, defaultConfig, defaultSystem, defineConfig, mergeConfigs } from "@chakra-ui/react"
 
+
+const theme = defineConfig({
+  theme: {
+    tokens: {
+      colors: {
+        gray: {
+          DEFAULT: { value: "#f9f9f9" },
+          100: { value: "#ededed" },
+        }
+      }
+    }
+  }
+}
+);
+
+const config = mergeConfigs(defaultConfig, theme);
+const system = createSystem(config);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider value={system}>
       <ColorModeProvider forcedTheme="dark">
         <App />
       </ColorModeProvider>
